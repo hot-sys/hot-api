e01982accbb998af5248220bfffa3d4087cc2bc6
Ratovonirina <eric.ratovonirina@gmail.com>
2025-01-07 10:03:35 +0300

    develop : fix issues in available room

--- hot_rooms/utils.py
+++ hot_rooms/utils.py
@@ -1,33 +1,37 @@
-from datetime import datetime, timedelta
-from django.utils.timezone import now, make_aware
 from .models import Room, CommandeRoom
+from datetime import timedelta
+from django.utils.timezone import now
 
 def update_room_status():
     today_datetime = now()
+    today_date = today_datetime.date()
     rooms = Room.objects.all()
 
     for room in rooms:
         overlapping_commandes = CommandeRoom.objects.filter(
             idRoom=room,
-            DateStart__lte=today_datetime,
-            DateEnd__gte=today_datetime
+            idStatus_id=3,
+            DateStart__date__lte=today_date,
+            DateEnd__date__gte=today_date
         )
 
-        room.available = not overlapping_commandes.exists()
+        if overlapping_commandes.exists():
+            room.available = False
 
-        upcoming_commandes = CommandeRoom.objects.filter(
-            idRoom=room
-        ).order_by('DateStart')
+            upcoming_commandes = CommandeRoom.objects.filter(
+                idRoom=room,
+                idStatus_id=3
+            ).order_by('DateStart')
 
-        if upcoming_commandes.exists():
-            current_date = today_datetime
+            current_date = today_date
             for commande in upcoming_commandes:
-                if commande.DateStart <= current_date <= commande.DateEnd:
-                    current_date = commande.DateEnd + timedelta(days=1)
-                elif commande.DateStart > current_date:
+                if commande.DateStart.date() <= current_date <= commande.DateEnd.date():
+                    current_date = commande.DateEnd.date() + timedelta(days=1)
+                elif commande.DateStart.date() > current_date:
                     break
-            room.dateAvailable = current_date.date()
+            room.dateAvailable = current_date
         else:
+            room.available = True
             room.dateAvailable = None
 
         room.save()
